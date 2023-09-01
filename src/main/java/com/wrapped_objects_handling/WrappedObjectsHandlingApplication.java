f7f63de6d4a85798183776cc312dc61ba5eb4c6b
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-31 21:57:15 -0600

    starting project wrapped_objects_handling

---
+++ src/main/java/com/wrapped_objects_handling/WrappedObjectsHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.wrapped_objects_handling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrappedObjectsHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrappedObjectsHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wrapped_objects_handling/WrappedObjectsHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrapped_objects_handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrappedObjectsHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
