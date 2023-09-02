50da4621357bb58ef1a6fbfceb705af3a50fa8d7
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-02 01:33:15 -0600

    implement method internalAddUser, return AddUserResult

--- src/main/java/com/wrapped_objects_handling/service/impl/UserServiceImpl.java
+++ src/main/java/com/wrapped_objects_handling/service/impl/UserServiceImpl.java
@@ -1,15 +1,26 @@
 package com.wrapped_objects_handling.service.impl;
 
 import com.wrapped_objects_handling.domain.User;
+import com.wrapped_objects_handling.repository.UserRepository;
 import com.wrapped_objects_handling.service.AddUserResult;
 import com.wrapped_objects_handling.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+  @Autowired
+  UserRepository userRepository;
+
   @Override
   public AddUserResult addUser(User user) {
     return null;
+  }
+
+  private AddUserResult internalAddUser(final User user){
+    final var saved = userRepository.save(user);
+    return addUserSuccessful(saved);
   }
 
 }
