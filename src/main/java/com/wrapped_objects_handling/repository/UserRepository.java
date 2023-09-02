47e40656966cc9a799366cecea155808e0ad373f
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-02 01:43:07 -0600

    implement method findByUsername and return Optional<User>

--- src/main/java/com/wrapped_objects_handling/repository/UserRepository.java
+++ src/main/java/com/wrapped_objects_handling/repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.wrapped_objects_handling.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, UUID> {
+
+  Optional<User> findByUsername(final String username);
+
 }
