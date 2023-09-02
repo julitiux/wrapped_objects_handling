730b8eee2ae8dc49ee458d9cc186a7e857434f15
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-02 01:37:05 -0600

    add logs using @Slf4j

--- src/main/java/com/wrapped_objects_handling/service/impl/UserServiceImpl.java
+++ src/main/java/com/wrapped_objects_handling/service/impl/UserServiceImpl.java
@@ -4,9 +4,11 @@ import com.wrapped_objects_handling.domain.User;
 import com.wrapped_objects_handling.repository.UserRepository;
 import com.wrapped_objects_handling.service.AddUserResult;
 import com.wrapped_objects_handling.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -19,12 +21,15 @@ public class UserServiceImpl implements UserService {
   }
 
   private AddUserResult internalAddUser(final User user) {
+    log.info("Trying to add user {}", user.getUsername());
     final var saved = userRepository.save(user);
+    log.info("Added user with Id: {}", saved.getId());
     return addUserSuccessful(saved);
   }
 
-  private AddUserResult handleExistingUser(User user) {
-    return duplicatedUserResult(user);
+  private AddUserResult handleExistingUser(User existingUser) {
+    log.warn("The username '{}' already exits.", existingUser.getUsername());
+    return duplicatedUserResult(existingUser);
   }
 
 }
