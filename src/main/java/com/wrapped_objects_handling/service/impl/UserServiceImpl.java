ad653ab51ab112d4647aec7533200d4e64557f28
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-02 01:31:00 -0600

    add UserService implement

---
+++ src/main/java/com/wrapped_objects_handling/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.wrapped_objects_handling.service.impl;
+
+import com.wrapped_objects_handling.domain.User;
+import com.wrapped_objects_handling.service.AddUserResult;
+import com.wrapped_objects_handling.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+  @Override
+  public AddUserResult addUser(User user) {
+    return null;
+  }
+
+}
