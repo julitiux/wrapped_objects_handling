0451466a27a91e2081656ef935ed8645cef45e7f
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-08 22:17:41 -0600

    implement method of and return a new object UserDTO

--- src/main/java/com/wrapped_objects_handling/webApi/UserWebApiMapper.java
+++ src/main/java/com/wrapped_objects_handling/webApi/UserWebApiMapper.java
@@ -2,10 +2,18 @@ package com.wrapped_objects_handling.webApi;
 
 import com.wrapped_objects_handling.domain.User;
 import com.wrapped_objects_handling.webApi.domain.AddUserCommand;
+import com.wrapped_objects_handling.webApi.domain.UserDTO;
+
+import java.util.Objects;
 
 public class UserWebApiMapper {
 
   public static User of(AddUserCommand addUserCommand){
     return new User(addUserCommand.username(), addUserCommand.password());
   }
+
+  public static UserDTO of(User user){
+    return new UserDTO(Objects.requireNonNull(user.getId()).toString(), user.getUsername());
+  }
+
 }
