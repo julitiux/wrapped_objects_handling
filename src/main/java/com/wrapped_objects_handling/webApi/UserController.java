93789af081c6e5ae2af1637b3d55b83d81b06687
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-08 22:18:13 -0600

    implement method addUser with a requestMapping /users

--- src/main/java/com/wrapped_objects_handling/webApi/UserController.java
+++ src/main/java/com/wrapped_objects_handling/webApi/UserController.java
@@ -1,4 +1,28 @@
 package com.wrapped_objects_handling.webApi;
 
+import com.wrapped_objects_handling.service.UserService;
+import com.wrapped_objects_handling.webApi.domain.AddUserCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+  @Autowired
+  UserService userService;
+
+  @PostMapping(value = "/users")
+  public ResponseEntity<Object> addUser(@RequestBody AddUserCommand addUserCommand) {
+    final var result = userService.addUser(UserWebApiMapper.of(addUserCommand));
+    if (result.isSuccess()) {
+      var user = result.getUser();
+      return new ResponseEntity<>(UserWebApiMapper.of(user), HttpStatus.CREATED);
+    }
+    return new ResponseEntity<>(result.getErrorMessage(), HttpStatus.BAD_REQUEST);
+  }
+
 }
